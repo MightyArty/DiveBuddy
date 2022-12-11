5902fc55c395b42085d86fab67aca271c77430e9
Artem Shabalin <tomshabalin95@gmail.com>
2022-12-11 13:22:18 +0200

    added dashboard

--- src/StartPage/Login.js
+++ src/StartPage/Login.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import Title from "../Title/Title";
+import { useNavigate } from "react-router-dom";
 
 export const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isValid, setIsValid] = useState(true);
+
+  const navigate = useNavigate();
+
+  const [authenticated, setAuthenticated] = useState(
+    localStorage.getItem(localStorage.getItem("authenticated") || false)
+  );
+
+  const users = [{ email: "example@gmail.com", password: "test1234" }];
 
   const emailChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
@@ -25,6 +34,12 @@ export const Login = (props) => {
     if (email.trim().length === 0 || password.trim().length === 0) {
       setIsValid(false);
       return;
+    }
+    const account = users.find((user) => user.email === email);
+    if (account && account.password === password) {
+      setAuthenticated(true);
+      localStorage.setItem("authenticated", true);
+      navigate("/dashboard");
     }
   };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Dashboard from "./Dashboard/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./StartPage/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>
 );
