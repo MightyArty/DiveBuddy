8f907693ab661d31ce92ae97ed99f126bfe1b16b
Artem Shabalin <tomshabalin95@gmail.com>
2022-12-26 19:01:19 +0200

    some

--- src/Forum/SendMessage.js
+++ src/Forum/SendMessage.js
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { auth } from "../firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import "./Forum.css";
-import {useRef} from 'react'
-import { getDocs, getFirestore} from 'firebase/firestore'
+import { useRef } from "react";
+import { getDocs, getFirestore } from "firebase/firestore";
 
 const style = {
   form: `h-14 w-full max-w-[728px]  flex text-xl absolute bottom-0`,
   input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
   button: `w-[20%] bg-red-500`,
-}
+};
 
 const SendMessage = () => {
-  const name= useRef();
+  const name = useRef();
   const [input, setInput] = useState("");
   /// getting name from database
   const db = getFirestore();
@@ -22,13 +22,13 @@ const SendMessage = () => {
       var user_data = doc.data();
       var user_email = user_data.email;
       if (user_email === auth.currentUser.email) {
-          name.current = user_data.name;
+        name.current = user_data.name;
       }
     });
   });
-  /// await beacuse we are talking to the firebase server but its gonna tale a littele bit time,
+
   const send_func = (event) => {
-    event.preventDefault(); ///  ומה שנכתב נעלם HTMLלמנוע את ההתנהגות הדיפולטיבית בשליחה טופס שהיא סימן שאלה בראש הדף כמו ב
+    event.preventDefault();
     const { uid, displayName } = auth.currentUser;
     addDoc(collection(db, "messages"), {
       text: input,
@@ -36,11 +36,9 @@ const SendMessage = () => {
       uid,
       timestamp: serverTimestamp(),
     });
+    setInput("");
   };
-  //By setting an onChange event handler, the handler function will get called whenever the text in the input field changes.
-  ///  !לפני השליחה עצמה אלא רק בעצם הכתיבה
-  // onChange is a listiner for the gray text window - setInput(event.target.value)} will work every time we write in the gray window
-  // when we press a button its fire a submit event . the onSubmit  defines a listiner function which work every time an event has fired
+
   return (
     <form onSubmit={send_func} className="form-forum">
       Enter your message
